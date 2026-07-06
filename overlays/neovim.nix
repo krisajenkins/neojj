5832d81193c95cc5e62496e04c84e821aa2d084f
Kris Jenkins <krisajenkins@gmail.com>
2026-07-06 12:44:47 +0100

    Updating to neovim 0.12.4.

--- overlays/neovim.nix
+++ overlays/neovim.nix
@@ -1,12 +1,13 @@
+# @update github-release neovim/neovim
 final: prev: {
   neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs (oldAttrs: rec {
-    version = "0.12.3";
+    version = "0.12.4";
 
     src = prev.fetchFromGitHub {
       owner = "neovim";
       repo = "neovim";
       rev = "v${version}";
-      hash = "sha256-JjDU3GZf+wvsMyDjIfu1btTUBkOlpp6E1HFLqBLR9po=";
+      hash = "sha256-KSLFsrnoEOV712cnUtA8s4EoISp+ON36jslKxSvDthQ=";
     };
   });
 }
